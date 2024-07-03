71127be00148091b0a5d84970462091df2d776d4
koral391 <koral391@mail.ru>
2024-07-03 21:43:13 +0500

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,8 @@
+numbers_homework = 12     #Количество выполненных заданий
+hours = 1.5                #Количество затраченных часов
+name = 'Python'           #название курса
+hour = (hours / numbers_homework) #Время на одно задание
+
+print(name)
+
+print('Курс: ' + name + ', всего задач:' + str(numbers_homework) + ', затрачено часов:' + str(hours) + ', среднее время выполнения ' +str(hour) + ' часов.')

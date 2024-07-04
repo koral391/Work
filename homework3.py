6cc1c06479fa5a1e1ae218a4bf5db55720c625ad
koral391 <koral391@mail.ru>
2024-07-04 19:09:51 +0500

    Add files via upload

---
+++ homework3.py
@@ -0,0 +1,9 @@
+name = 'Ahat'
+age = 26
+new_age = 27
+is_student = age < new_age
+
+print('Name: ' + name)
+print('Age: ' + str(age))
+print('New Age: ' + str(new_age))
+print('Is Student: ' + str(is_student))

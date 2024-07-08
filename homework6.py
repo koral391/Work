8b1ce53c0cb003c3aa633c0ad3c187860a81127b
koral391 <koral391@mail.ru>
2024-07-08 21:55:18 +0500

    Add files via upload

---
+++ homework6.py
@@ -0,0 +1,19 @@
+my_friand = { 'Даниил': 2001, 'Никита': 1999, 'Вера': 1999 }
+print(my_friand)
+
+print(my_friand.get('Никита'))
+my_friand ['Рустам'] = 1997
+print(my_friand)
+
+my_friand. update({'Олег': 2004, 'Салих': 1998})
+print(my_friand)
+del my_friand ['Даниил']
+print(my_friand)
+
+
+my_set = {1,2,8,10,15,2,8,15,10}
+print(my_set)
+print(my_set.add(99))
+print(my_set.add(78))
+print(my_set.remove(8))
+print(my_set)
